c2537a23401d28fe19964c0f8cbb7cec73442664
mrking <1779417490@qq.com>
2018-09-17 17:56:43 +0800

    mrking

--- 爬虫/18.py
+++ 爬虫/18.py
@@ -16,10 +16,14 @@ import json
 
 sta = 0
 l1 = list()
-for j in range(100):
+id = 100
 
+print('开始爬取')
 
-    url = "https://movie.douban.com/j/chart/top_list?type=11&interval_id=100%3A90&action=&start={0}&limit=20".format(sta)
+for j in range(1000):
+
+    url = 'https://movie.douban.com/j/chart/top_list?type=6&interval_id={1}%3A{2}&action=&start={0}&limit=20'.format(sta,id,id-10)
+    #url = "https://movie.douban.com/j/chart/top_list?type=11&interval_id={1}%3A{2}&action=&start={0}&limit=20".format(sta,id,id-10)
 
     rsp = request.urlopen(url)
 
@@ -29,7 +33,8 @@ for j in range(100):
     data = json.loads(data)
 
     if len(data) > 0:
-        #print('一共有{0}个元素'.format(len(data)))
+
+        print('已经爬取了{0}个元素'.format(len(data)))
         for i in data:
             #print(i['title'] +'-------' +  i['url'])
             l1.append(i['title'] +'-------' +  i['url'])
@@ -37,10 +42,18 @@ for j in range(100):
 
         sta = sta + 20
     else:
-        break
+
+        if id > 10:
+            print('开始爬取{0}到{1}之间的电影'.format(id - 5, id - 15))
+            id = id - 5
+            sta = 0
+
+        else:
+            break
 
 # 添加到列表中后开始写入文件
 
+print('爬取完毕，开始写入')
 file = open('排行榜.txt','a')
 
 for i in l1:
@@ -50,3 +63,4 @@ file.close()
 
 print('一共有{0}条电影数据'.format(len(l1)))
 print('写入完毕')
+
